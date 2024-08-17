3c1c309451db664a27452984bc375761285779a1
Yogesh Palve <dnyyogesh@gmail.com>
2024-08-17 23:59:28 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1>hello this side</h1>
+      <Button>Hello button</Button>
+    </>
+  );
+}
